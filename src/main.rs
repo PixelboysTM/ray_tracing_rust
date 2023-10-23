ef1b816951906f4a52c7036e080cb446fc7c7a79
Tim Fritzen <fritzentim10@gmail.com>
2023-10-23 13:53:44 +0200

    Added Simple Light

--- src/main.rs
+++ src/main.rs
@@ -9,34 +9,45 @@ use std::time::Instant;
 
 use hit::{Hit, World};
 use rand::Rng;
+use vec::Vec3;
 
 use crate::camera::Camera;
 use crate::ray::Ray;
 use crate::sphere::Sphere;
 use crate::vec::Color;
 
-fn ray_color(r: &Ray, world: &World) -> Color {
-    if let Some(rec) = world.hit(r, 0.0, f64::INFINITY) {
-        0.5 * (rec.normal + Color::new(1.0, 1.0, 1.0))
+fn ray_color(r: &Ray, world: &World, depth: u64) -> Color {
+    if depth <= 0 {
+        return Color::new(0.0, 0.0, 0.0);
+    }
+
+    if let Some(rec) = world.hit(r, 0.001, f64::INFINITY) {
+        // let target = rec.p + rec.normal + Vec3::random_in_unit_sphere().normalized();
+        let target = rec.p + Vec3::random_in_hemisphere(rec.normal);
+        let r = Ray::new(rec.p, target - rec.p);
+
+        0.5 * ray_color(&r, world, depth - 1)
     } else {
         let unit_direction = r.direction().normalized();
         let t = 0.5 * (unit_direction.y() + 1.0);
         (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
     }
 }
-///https://misterdanb.github.io/raytracinginrust/#diffusematerials
+///https://misterdanb.github.io/raytracinginrust/#metal
 fn main() {
     let timer = Instant::now();
 
     // Image
     const ASPECT_RATIO: f64 = 16.0 / 9.0;
-    const IMAGE_WIDTH: u64 = 1920;
+    const IMAGE_WIDTH: u64 = 512;
     const IMAGE_HEIGHT: u64 = ((IMAGE_WIDTH as f64) / ASPECT_RATIO) as u64;
-    const SAMPLES_PER_PIXEL: u64 = 50;
+    const SAMPLES_PER_PIXEL: u64 = 100;
+    const MAX_DEPTH: u64 = 5;
 
     // World
     let mut world = World::new();
     world.push(Sphere::new((0.0, 0.0, -1.0).into(), 0.5).wrap());
+    world.push(Sphere::new((0.2, 1.2, -1.5).into(), 0.15).wrap());
     world.push(Sphere::new((0.0, -100.5, -1.0).into(), 100.0).wrap());
 
     // Camera
@@ -48,7 +59,7 @@ fn main() {
 
     let mut rng = rand::thread_rng();
     for j in (0..IMAGE_HEIGHT).rev() {
-        eprint!("\rScanlines remaining: {:3} ", j);
+        eprint!("\rScanlines remaining: {:3}/{:3} ", j, IMAGE_HEIGHT);
         stderr().flush().unwrap();
 
         for i in 0..IMAGE_WIDTH {
@@ -61,7 +72,7 @@ fn main() {
                 let v = (j as f64 + random_v) / ((IMAGE_HEIGHT - 1) as f64);
 
                 let r = cam.get_ray(u, v);
-                pixel_color += ray_color(&r, &world);
+                pixel_color += ray_color(&r, &world, MAX_DEPTH);
             }
             println!("{}", pixel_color.format_color(SAMPLES_PER_PIXEL));
         }
