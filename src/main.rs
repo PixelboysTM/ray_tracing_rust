0d72ea89fc586226cc2319ed38044bd699ad69d2
Tim Fritzen <fritzentim10@gmail.com>
2023-10-26 11:17:48 +0200

    Chapter 01

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,36 @@ mod tuples;
 
 fn main() {
     println!("Hello World");
+    pit_01::main();
+}
+
+mod pit_01 {
+    use crate::tuples::{
+        helpers::{point, vector},
+        Tuple,
+    };
+
+    fn tick(env: (Tuple, Tuple), proj: (Tuple, Tuple)) -> (Tuple, Tuple) {
+        let position = proj.0 + proj.1;
+        let velocity = proj.1 + env.0 + env.1;
+        return (position, velocity);
+    }
+
+    pub fn main() {
+        let mut p = (point(0, 1, 0), vector(1, 1, 0).normalized());
+        let e = (vector(0, -0.1, 0), vector(-0.01, 0, 0));
+
+        let mut ticker = 0;
+        loop {
+            ticker += 1;
+            if p.0.y() <= 0.0 {
+                break;
+            }
+            p = tick(e, p);
+
+            println!("Tick {ticker} at {:?}", p.0);
+        }
+
+        println!("It took {ticker} Ticks.")
+    }
 }
--- src/tuples.rs
+++ src/tuples.rs
@@ -1,6 +1,9 @@
-use std::ops::{Add, AddAssign, Sub};
+use std::{
+    fmt::{write, Debug},
+    ops::{Add, Div, Mul, Neg, Sub},
+};
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy)]
 pub struct Tuple {
     x: f64,
     y: f64,
@@ -40,11 +43,49 @@ impl Tuple {
         Tuple::new(x, y, z, 0.0)
     }
 
+    pub fn x(&self) -> f64 {
+        self.x
+    }
+    pub fn y(&self) -> f64 {
+        self.y
+    }
+    pub fn z(&self) -> f64 {
+        self.z
+    }
+    pub fn w(&self) -> f64 {
+        self.w
+    }
+
     pub fn is_point(&self) -> bool {
         self.w.eps_eq(&1.0)
     }
     pub fn is_vector(&self) -> bool {
         self.w.eps_eq(&0.0)
+    }
+    pub fn magnitude(&self) -> f64 {
+        (self.x * self.x + self.y * self.y + self.z * self.z + self.w * self.w).sqrt()
+    }
+    pub fn normalized(&self) -> Tuple {
+        *self * (1.0 / self.magnitude())
+    }
+    pub fn dot(&self, b: &Self) -> f64 {
+        self.x * b.x + self.y * b.y + self.z * b.z + self.w * b.w
+    }
+    pub fn cross(&self, other: &Self) -> Tuple {
+        assert!(self.w.eps_eq(&0.0));
+        assert!(other.w.eps_eq(&0.0));
+
+        Tuple::vector(
+            self.y * other.z - self.z * other.y,
+            self.z * other.x - self.x * other.z,
+            self.x * other.y - self.y * other.x,
+        )
+    }
+}
+
+impl Debug for Tuple {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({},{},{},{})", self.x(), self.y(), self.z(), self.w())
     }
 }
 
@@ -83,17 +124,75 @@ impl Sub for Tuple {
     }
 }
 
+impl Neg for Tuple {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        Tuple::new(0, 0, 0, 0) - self
+    }
+}
+
+impl Mul<f64> for Tuple {
+    type Output = Self;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        helpers::tuple(self.x * rhs, self.y * rhs, self.z * rhs, self.w * rhs)
+    }
+}
+
+impl Div<f64> for Tuple {
+    type Output = Self;
+
+    fn div(self, rhs: f64) -> Self::Output {
+        self * (1.0 / rhs)
+    }
+}
+
+const EPSILON: f64 = 0.00001;
 trait FEquals {
     fn eps_eq(&self, rhs: &Self) -> bool;
 }
 impl FEquals for f64 {
     fn eps_eq(&self, rhs: &Self) -> bool {
-        (self - rhs).abs() < f64::EPSILON
+        (self - rhs).abs() < EPSILON
+    }
+}
+
+pub mod helpers {
+    use super::Tuple;
+
+    pub fn tuple<F1, F2, F3, F4>(x: F1, y: F2, z: F3, w: F4) -> Tuple
+    where
+        F1: Into<f64>,
+        F2: Into<f64>,
+        F3: Into<f64>,
+        F4: Into<f64>,
+    {
+        Tuple::new(x, y, z, w)
+    }
+
+    pub fn point<F1, F2, F3>(x: F1, y: F2, z: F3) -> Tuple
+    where
+        F1: Into<f64>,
+        F2: Into<f64>,
+        F3: Into<f64>,
+    {
+        Tuple::point(x, y, z)
+    }
+
+    pub fn vector<F1, F2, F3>(x: F1, y: F2, z: F3) -> Tuple
+    where
+        F1: Into<f64>,
+        F2: Into<f64>,
+        F3: Into<f64>,
+    {
+        Tuple::vector(x, y, z)
     }
 }
 
 #[cfg(test)]
 mod tests {
+    pub use super::helpers::*;
     use super::*;
 
     #[test]
@@ -156,5 +255,86 @@ mod tests {
         let v1 = Tuple::vector(3, 2, 1);
         let v2 = Tuple::vector(5, 6, 7);
         assert_eq!(v1 - v2, Tuple::vector(-2, -4, -6));
+    }
+
+    #[test]
+    fn subtracting_vector_from_zero() {
+        let zero = vector(0, 0, 0);
+        let v = vector(1, -2, 3);
+        assert_eq!(zero - v, vector(-1, 2, -3));
+    }
+
+    #[test]
+    fn negating_a_tuple() {
+        let a = tuple(1, -2, 3, -4);
+        assert_eq!(-a, tuple(-1, 2, -3, 4));
+    }
+
+    #[test]
+    fn mul_tuple_scalar() {
+        let a = tuple(1, -2, 3, -4);
+        assert_eq!(a * 3.5, tuple(3.5, -7, 10.5, -14));
+    }
+
+    #[test]
+    fn mul_tuple_fraction() {
+        let a = tuple(1, -2, 3, -4);
+        assert_eq!(a * 0.5, tuple(0.5, -1, 1.5, -2));
+    }
+
+    #[test]
+    fn div_tuple_scalar() {
+        let a = tuple(1, -2, 3, -4);
+        assert_eq!(a / 2.0, tuple(0.5, -1, 1.5, -2));
+    }
+
+    #[test]
+    fn computing_magnitude() {
+        let v = vector(1, 0, 0);
+        assert_eq!(v.magnitude(), 1.0);
+        let v = vector(0, 1, 0);
+        assert_eq!(v.magnitude(), 1.0);
+        let v = vector(0, 0, 1);
+        assert_eq!(v.magnitude(), 1.0);
+
+        let v = vector(1, 2, 3);
+        assert_eq!(v.magnitude(), 14.0_f64.sqrt());
+        let v = vector(-1, -2, -3);
+        assert_eq!(v.magnitude(), 14.0_f64.sqrt());
+    }
+
+    #[test]
+    fn normalization_vector() {
+        let v = vector(4, 0, 0);
+        assert_eq!(v.normalized(), vector(1, 0, 0));
+
+        let v = vector(1, 2, 3);
+        assert_eq!(
+            v.normalized(),
+            vector(
+                1.0 / 14_f64.sqrt(),
+                2.0 / 14_f64.sqrt(),
+                3.0 / 14_f64.sqrt()
+            )
+        );
+
+        let norm = v.normalized();
+        assert!(norm.magnitude().eps_eq(&1.0));
+    }
+
+    #[test]
+    fn dot_of_tuples() {
+        let a = vector(1, 2, 3);
+        let b = vector(2, 3, 4);
+
+        assert!(a.dot(&b).eps_eq(&20.0));
+    }
+
+    #[test]
+    fn cross_of_vectors() {
+        let a = vector(1, 2, 3);
+        let b = vector(2, 3, 4);
+        assert_eq!(a.cross(&b), vector(-1, 2, -1));
+        assert_eq!(b.cross(&a), vector(1, -2, 1));
     }
 }
